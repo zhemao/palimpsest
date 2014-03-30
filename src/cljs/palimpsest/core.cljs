7956f56d268f73a1e88ca2d34f6163ebe3b588d8
Howard Mao <zhehao.mao@gmail.com>
2014-03-29 18:00:33 -0700

    initialize project template

---
+++ src/cljs/palimpsest/core.cljs
@@ -0,0 +1,17 @@
+;;; If this namespace requires macros, remember that ClojureScript's
+;;; macros are written in Clojure and have to be referenced via the
+;;; :require-macros directive where the :as keyword is required, while in Clojure is optional. Even
+;;; if you can add files containing macros and compile-time only
+;;; functions in the :source-paths setting of the :builds, it is
+;;; strongly suggested to add them to the leiningen :source-paths.
+(ns palimpsest.core)
+
+(defn foo [greeting]
+  (if greeting
+    (str greeting "ClojureScript!")
+    (str "Hello, ClojureScript!")))
+
+(.write js/document (foo "Welcome to "))
+
+
+
